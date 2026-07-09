79bb8668e73a1671aa1cb1dca30ea3698752d9b9
hamohamamo <hhhaaammmaaammmooo@gmail.com>
2026-07-09 13:17:12 +0300

    add Majority Element.py

---
+++ Majority Element.py
@@ -0,0 +1,16 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count += 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate

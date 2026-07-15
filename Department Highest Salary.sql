794cce5b36eaae7f0c6457b1743648ff18fff347
hamohamamo <hhhaaammmaaammmooo@gmail.com>
2026-07-15 18:36:45 +0300

    add Department Highest Salary.sql

---
+++ Department Highest Salary.sql
@@ -0,0 +1,11 @@
+SELECT
+    D.name AS Department,
+    E.name AS Employee,
+    E.salary AS Salary
+FROM
+    Employee E
+JOIN
+    Department D ON E.departmentId = D.id
+WHERE
+    (E.departmentId, Salary) IN 
+        (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId);

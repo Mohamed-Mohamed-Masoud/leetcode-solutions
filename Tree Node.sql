70652be388e42a47cea6bc3ef4a1a2493275f395
hamohamamo <hhhaaammmaaammmooo@gmail.com>
2026-08-09 12:23:52 +0300

    Add: [SQL] Tree Node

---
+++ Tree Node.sql
@@ -0,0 +1,20 @@
+"""
+Intuition
+To classify each node in a tree, we can determine its position by checking its parent ID (p_id) and seeing whether it serves as a parent to any other nodes in the table.
+
+Approach
+1. Use a CASE statement to evaluate conditions sequentially for each node.
+2. Root: If a node has no parent (p_id IS NULL), it is the root of the tree.
+3. Inner: If a node's id is present in the p_id column of the table (id IN (SELECT p_id FROM Tree)), it means it has children, making it an inner node.
+4. Leaf: If neither of the above conditions is met, the node has a parent but no children, meaning it is a leaf node.
+
+Code
+"""
+SELECT
+    id,
+    CASE
+        WHEN p_id is null THEN 'Root'
+        WHEN id in (SELECT p_id FROM TREE) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree;
--- Who Has the Most Friends.sql
+++ Who Has the Most Friends.sql
@@ -3,15 +3,11 @@ Intuition
 To find the user with the most friends, we must count every time a user is part of an accepted request. Since a user can be either the one who sent the request or the one who accepted it, we need to look at both columns combined.
 
 Approach
-Use UNION ALL to merge both requester_id and accepter_id into a single, unified list of IDs.
-
-Group this combined list by id.
-
-Use COUNT(*) to calculate the total number of friends for each user.
-
-Sort the results in descending order based on the count (ORDER BY num DESC).
-
-Use LIMIT 1 to extract only the top user with the highest number of friends.
+1. Use UNION ALL to merge both requester_id and accepter_id into a single, unified list of IDs.
+2. Group this combined list by id.
+3. Use COUNT(*) to calculate the total number of friends for each user.
+4. Sort the results in descending order based on the count (ORDER BY num DESC).
+5. Use LIMIT 1 to extract only the top user with the highest number of friends.
 
 Code
 """
